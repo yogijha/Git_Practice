36e4e1fc6c33921db70aca7c9bc26bac45b85ea8
YOGESH KUMAR <yogeshkjha23@gmail.com>
2021-06-07 17:33:58 +0530

    modified hello world text.

--- Git_Practice/Program.cs
+++ Git_Practice/Program.cs
@@ -6,7 +6,7 @@ namespace Git_Practice
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Modified Branch!");
             Console.ReadKey();
         }
     }
